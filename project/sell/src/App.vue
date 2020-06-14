fea17e1a3ea36269587165d26580659663b020fb
HMTT <824626339@qq.com>
2020-06-14 16:40:42 +0800

    修改乱报错

--- project/sell/src/App.vue
+++ project/sell/src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+
+  name: 'App',
+  compoments:{
+
+  }
 }
 </script>
 
---
+++ project/sell/src/components/RenLin/DongTai.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="dongTai">
+        动态
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
